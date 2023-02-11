4bc841f1dab4617a6f82488ea063301c051bc2fa
Andrea Bianchi <andrea@kaist.ac.kr>
2023-02-11 19:24:38 +0900

    lvim/config wrapping text

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -221,7 +221,61 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 
+-- Adding util to create and delete a scratch buffer
 lvim.builtin.which_key.mappings['b'].s = {'<cmd>enew<cr>', 'Scratch'}
 lvim.builtin.which_key.mappings['b'].k = {'<cmd>bd<cr>', 'Kill buffer'}
 
 
+-- Wrapping in text mode
+vim.g.wrap = false
+vim.api.nvim_create_user_command('ToggleWrap',
+  function()
+    if not vim.g.wrap then
+
+      vim.g.wrap= true
+      vim.wo.wrap= true
+      vim.wo.linebreak= true
+
+      vim.api.nvim_set_keymap('n', '<Up>', 'gk', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Down>', 'gj', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Home>', 'g<Home>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<End>', 'g<End>', { noremap = true, silent = true })
+    
+    else
+    
+      vim.g.wrap= false
+      vim.wo.wrap= false 
+      vim.wo.linebreak= false
+    
+      vim.api.nvim_del_keymap('n', '<Up>')
+      vim.api.nvim_del_keymap('n', '<Down>')
+      vim.api.nvim_del_keymap('n', 'j')
+      vim.api.nvim_del_keymap('n', 'k')
+      vim.api.nvim_del_keymap('n', '<Home>')
+      vim.api.nvim_del_keymap('n', '<End>')
+    end
+
+  end,
+  { nargs = 0 })
+
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.txt", "*.md", "*.json", ".jsonc" },
+  -- enable wrap mode for selected files only
+  command = "ToggleWrap",
+})
+
+lvim.builtin.which_key.mappings['u'].w = {'<cmd>ToggleWrap<cr>', 'Toggle Wrap'}
+
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.md", "*.txt" },
+--   -- enable wrap mode for json files only
+--   command = "ToggleWrap",
+-- -- vim.api.nvim_create_autocmd("BufEnter", {
+-- --   pattern = { "*.json", "*.jsonc" },
+-- --   -- enable wrap mode for json files only
+-- --   command = "setlocal wrap",
+-- -- })
+-- })
