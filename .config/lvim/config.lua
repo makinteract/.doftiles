df7adda942e76a15523a4e29726ded6b516334ba
Andrea Bianchi <andrea@kaist.ac.kr>
2023-02-12 15:45:33 +0900

    Emmet keybindings

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -207,8 +207,10 @@ lvim.plugins = {
 -- Emmet
 
 vim.g.user_emmet_install_global = 0
-vim.g.user_emmet_leader_key=','
+-- vim.g.user_emmet_leader_key=','
 vim.g.user_emmet_mode='a'
+-- Activate Emmet with a tab
+vim.g.user_emmet_expandabbr_key = '<Tab>';
 
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.css", "*.html" },
