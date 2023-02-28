efec7dcdf55c95263782d1b1b54d3dd2c02e3289
Andrea Bianchi <andrea@kaist.ac.kr>
2023-02-28 21:27:39 +0900

    Adding lsp_signature

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -203,9 +203,19 @@ lvim.plugins = {
   },
   {
     "junegunn/fzf"
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    config = function()
+      require "lsp_signature".setup({
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+        handler_opts = {
+          border = "rounded"
+        }
+      })
+    end
   }
 }
-
 
 -- Emmet
 
